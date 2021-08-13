cc0ce6b5b06498b5f7983e17c75706b1eb40a092
Ekaterina Gogoleva <goglevaekaterina17@gmail.com>
2021-08-13 12:31:38 +0300

    components loginform, myprofile, regform, search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { LoginformComponent } from './loginform/loginform.component';
+import { MyprofileComponent } from './myprofile/myprofile.component';
+import { SearchComponent } from './search/search.component';
+import { RegformComponent } from './regform/regform.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LogInComponent,
+    LoginformComponent,
+    MyprofileComponent,
+    SearchComponent,
+    RegformComponent
   ],
   imports: [
     BrowserModule,
