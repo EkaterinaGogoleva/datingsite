6967e38aed3d9e3d8a12c18d078b872a4f9e67ce
Ekaterina Gogoleva <goglevaekaterina17@gmail.com>
2021-08-15 23:20:25 +0300

    auth.service, profile.service, profile, auth.guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component'
 import { LoginformComponent } from './loginform/loginform.component';
 import { MyprofileComponent } from './myprofile/myprofile.component';
 import { SearchComponent } from './search/search.component';
 import { RegformComponent } from './regform/regform.component';
 import { StartComponent } from './start/start.component';
+import { AuthGuard } from './auth.guard';
 
 @NgModule({
   declarations: [
@@ -19,8 +22,13 @@ import { StartComponent } from './start/start.component';
     RegformComponent,
     StartComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [
+    BrowserModule, 
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule],
+  providers: [AuthGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
