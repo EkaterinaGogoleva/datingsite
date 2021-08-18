25debf7fe46cc142b21025183f4e5f194f678425
Ekaterina Gogoleva <goglevaekaterina17@gmail.com>
2021-08-19 00:06:30 +0300

    регистрация завершена по туториалу 1 и 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginformComponent } from './loginform/loginform.component';
-import { MyprofileComponent } from './myprofile/myprofile.component';
-import { RegformComponent } from './regform/regform.component';
-import { SearchComponent } from './search/search.component';
-import { StartComponent } from './start/start.component';
+
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
 
 const routes: Routes = [
-  { path: '',   redirectTo: '/start', pathMatch: 'full' }, // redirect to `first-component`
-  { path: 'myprofile', component: MyprofileComponent /*, canActivate: [AuthGuard] */ },
-  //reform on suojattu LoginGuardilla
-  { path: 'regform', component: RegformComponent  },
-  { path: 'loginform', component: LoginformComponent },
-  { path: 'search', component: SearchComponent /*, canActivate: [AuthGuard] */ },
-  { path: 'start', component: StartComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'user', component: BoardUserComponent },
+  { path: 'mod', component: BoardModeratorComponent },
+  { path: 'admin', component: BoardAdminComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+/*Tutorial 2
+First, we check isLoggedIn status using TokenStorageService, 
+if it is true, we get user’s roles and set value 
+for showAdminBoard & showModeratorBoard flag. They will 
+control how template navbar displays its items. */
+import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from './_services/token-storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'datingsite';
+export class AppComponent implements OnInit {
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username?: string;
+
+  constructor(private tokenStorageService: TokenStorageService) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.username;
+    }
+  }
+
+  logout(): void {
+    this.tokenStorageService.signOut();
+    window.location.reload();
+  }
 }
