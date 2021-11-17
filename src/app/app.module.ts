e6a99a18ad61b42801e0abc0d78d4b24e17c0352
Ekaterina Gogoleva <goglevaekaterina17@gmail.com>
2021-11-17 10:38:04 +0200

    before firebase init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,15 @@ import { MaterialModule } from './material.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireMessagingModule } from '@angular/fire/messaging';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireFunctionsModule } from '@angular/fire/functions';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,6 +40,13 @@ import { environment } from '../environments/environment';
    
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFireMessagingModule,
+    AngularFireDatabaseModule,
+    AngularFireFunctionsModule,
     BrowserModule, 
     HttpClientModule,
     AppRoutingModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+   firebaseConfig: {
+    apiKey: "AIzaSyDNzQ_1jz97bmeBmFg5r-ulIcJaMU4nQiE",
+    authDomain: "datingsiteang.firebaseapp.com",
+    projectId: "datingsiteang",
+    storageBucket: "datingsiteang.appspot.com",
+    messagingSenderId: "112758228407",
+    appId: "1:112758228407:web:f8865e904461a3c6505432",
+    measurementId: "G-800TMH9GNH"
+  },
   production: false
 };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 
 /*
  * For easier debugging in development mode, you can import the following file
